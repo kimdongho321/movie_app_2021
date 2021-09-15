c94ac5b12a00dced4a4c1d56380563d8993e071d
kimdongho <ehdrh321@gmail.com>
2021-09-15 10:23:40 +0900

    props 연습 6, 7, 8, 9, 10 줄 생성

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
+
 function App() {
   return (
     <div className="App">
       <h1>Hello React</h1>
-      <Potato />
+      <Movie />
+      <Movie />
+      <Movie />
+      <Movie />
+      <Movie />
     </div>
   );
 }
 
-function Potato() {
+function Movie() {
   return (
   <h3>I love potato</h3> //JSX문법
     );
   }
 
-export default App
+export default App //꼭 써야 외부에서 사용가능
