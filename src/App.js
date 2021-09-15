b57db36dbd264992ebbc2e164764e4dcf9041c5f
kimdongho <ehdrh321@gmail.com>
2021-09-15 10:43:48 +0900

    props 연습 13줄 수정

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ function App() {
   return (
     <div className="App">
       <h1>Hello React</h1>
-      <Food fav="kimchi"/>
+      <Food fav="kimchi" something={true} papapa={['hello', 1, 2, true]}/> 
     </div>
   );
 }
 
-function Food() {
+function Food(props) {
   return (
-  <h3>I love potato</h3> //JSX문법
+  <h3>I love {props.fav}</h3> //JSX문법
     );
   }
 
