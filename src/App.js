bc6f376196e103f4423e1654df89341aff24458b
kimdongho <ehdrh321@gmail.com>
2021-09-29 12:29:04 +0900

    12~20줄 add,minus 추가 25~27 버튼 추가

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import React, {Component} from 'react'
 
 class App extends Component{
+    constructor(props){
+        super(props)
+        console.log('constructor')
+    }
+    state = {
+        count:0
+    }
+
+    add = () => {
+        console.log('add')
+        this.setState(current => ({count: this.state.count +1}))
+    }
+  
+    minus = () => {
+        console.log('minus')
+        this.setState(current => ({count: this.state.count -1}))
+    }
   render() {
       return (
-          <h1>I am a class component</h1>
-      )
+          <div>
+          <h1>The number is: {this.state.count}</h1>
+            <button onClick={this.add}>Add</button>
+            <button onClick={this.minus}>Minus</button>
+            <button>Add</button>
+            </div>
+          )
   }
 }
 
