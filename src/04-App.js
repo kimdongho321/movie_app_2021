43a631a5084a1af928cde37242c11b5b6a6795de
kimdongho <ehdrh321@gmail.com>
2021-09-29 11:51:10 +0900

    2교시 종료

--- src/04-App.js
+++ src/04-App.js
@@ -4,14 +4,14 @@ const foodlike =[
   {
     id: 1,
     name: 'Pizza',
-    Image: 'https://cdn.dominos.co.kr/admin/upload/goods/20200311_x8StB1t3.jpg',
+    Image: '/img/pizza.jpg',
     alt: '피자',
     rating: 4.5
   },
   {
     id: 2,
     name: 'bugger',
-    Image: 'https://cdn.eyesmag.com/content/uploads/posts/2020/09/28/fast-food-franchises-serve-hamburger-without-tomatoes-01-52f4f2a6-a73a-4abc-ba3c-419ee2660639.jpg',
+    Image: '/img/bugger.jpg',
     alt: '햄버거',
     rating: 5.0
   }
@@ -40,6 +40,7 @@ Food.propTypes={
     picture: PropTypes.string.isRequired,
     rating: PropTypes.number
   }
+
   function Food({name, picture, alt, rating}) {
     return (
      <div>
